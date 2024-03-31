2589457ac5c26532b8ba2841a0b1f0361f8025d6
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-03-31 13:37:19 +0500

    Final Commit

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
-const mongoose = require('mongoose');
+// server.js (or app.js)
+const emailRoutes = require('./Routes/emailRoutes');
 const express = require('express');
-const router = express.Router();
-const jwt = require('jsonwebtoken');
-
-const UserRouter = require('./Routes/user');
 const app = express();
 const cors = require('cors');
-const env = require('dotenv').config();
-
-app.use(cors());
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/user', UserRouter);
+// Routes
+app.use('/api/email', emailRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/voyagebuddy')
-
-    .then(() => {
-        console.log('Connected to database');
-    }
-    )
-    .catch(() => {
-        console.log('Connection failed');
-    }
-    );
-
-app.listen(
-    process.env.PORT || 3000, () => {
-        console.log('Server started');
-    }
-);
-
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
---
+++ Services/nodeMailerService.js
@@ -0,0 +1,32 @@
+// emailService.js
+
+const { createTransport } = require('nodemailer');
+const password = process.env.REACT_APP_SMTP_PASSWORD;
+const email_Host = process.env.REACT_APP_SMTP_EMAIL;
+
+const sendMailUsingSMTP = async (email, OTP) => {
+    const transporter = createTransport({
+        service: 'gmail',   
+        auth: {
+          user: "sohaibsipra869@gmail.com",
+          pass: "xeri feke fkog scdn"
+        }
+      });
+
+    const mailOptions = {
+        from: email_Host,
+        to: email,
+        subject: 'OTP for email verification',
+        text: `Your OTP is ${OTP}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+module.exports = sendMailUsingSMTP;
